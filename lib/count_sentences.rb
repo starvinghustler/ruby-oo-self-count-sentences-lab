ce1394a532cabdc2fd020f4a7b13eb79c1aab7ff
idobok <idobok12@gmail.com>
2021-11-22 20:30:10 +0200

    done

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,20 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    self.split(/\.|\?|\!/).delete_if {|x| x.size < 2}.size     #splits in every point of where delimiter is. 
+                                                          #because of spaces and double !, we need to remove conflicting results
+                                                          #size turns the indexes into a count
   end
 end
